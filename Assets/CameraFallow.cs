c1a0897c1cc77c3be7c372ab83b2e6d339cd1149
hyun19899 <hyun19899@naver.com>
2019-03-07 12:42:25 +0900

    해상도 9:16 세로버전
    해상도 9:16 / 빌드셋팅 Android / 플레이어 이동 /
    버튼다운시 해당방향으로 회전 / 버튼 업 회전정지 /
    카메라가 플레이어 따라가는것만 구현 /
    
    카메라 회전(노구현)

---
+++ Assets/CameraFallow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraFallow : MonoBehaviour
+{
+    public Transform target;
+    private Vector3 offset = Vector3.zero;
+
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+        offset = transform.position - target.position;
+        transform.position = new Vector3(
+            target.position.x,
+            target.position.y + offset.y,
+            target.position.z);
+    }
+}
---
+++ Assets/Move.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    private Coroutine left;
+    private Coroutine right;
+
+    //코루틴 변수 = 코루틴 실행
+
+    //스탑코루틴(코루틴 변수)
+
+    private void Awake()
+    {
+        Debug.Log("게임시작");
+        StartCoroutine(Run());
+    }
+
+    private IEnumerator Run()
+    {
+        for (; ; )
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    public void leftbutton()
+    {
+        Debug.Log("왼쪽 회전");
+        LeftCuc();
+    }
+
+    public void rightbutton()
+    {
+        Debug.Log("오른쪽 회전");
+        RightCuc();
+    }
+
+    public void leftbuttonUp()
+    {
+        StopCoroutine(left);
+    }
+
+    public void rightbuttonUp()
+    {
+        StopCoroutine(right);
+    }
+
+    private void LeftCuc()
+    {
+        left = StartCoroutine(LeftRotCorutine());
+    }
+
+    private void RightCuc()
+    {
+        right = StartCoroutine(RightRotCorutine());
+    }
+
+    private IEnumerator LeftRotCorutine()
+    {
+        for (; ; )
+        {
+            transform.rotation *= Quaternion.Euler(Vector3.up * -1.0f);
+            yield return null;
+        }
+    }
+
+    private IEnumerator RightRotCorutine()
+    {
+        for (; ; )
+        {
+            transform.rotation *= Quaternion.Euler(Vector3.up * 1.0f);
+            yield return null;
+        }
+    }
+}
