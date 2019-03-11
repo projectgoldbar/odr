812221b4c54f9a2041be980590a107d1a87d9b39
wkdeo12 <43337006+wkdeo12@users.noreply.github.com>
2019-03-11 18:11:05 +0900

    아이템사용기능
    아이템 아이콘도 변경됨

--- Assets/DD/Scripts/Item.cs
+++ Assets/DD/Scripts/Item.cs
@@ -1,18 +1,52 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Item : MonoBehaviour
 {
+    public Sprite itemImg;
+
+    private Transform playervecter;
+
     private void OnCollisionEnter(Collision other)
     {
-        other.gameObject.GetComponent<Inventory>().inventory.Enqueue(this);
-        //other.gameObject.GetComponent<Inventory>().inven.Add(this);
+        var x = other.gameObject.GetComponent<Inventory>();
+
+        if (x != null)
+        {
+            playervecter = x.transform;
+            //x.ItemChanged = () => { x.itemImage.sprite = itemImg; };
+            if (x.inven.Count > 3)
+            {
+                x.inven[0] = this;
+                //Instantiate(itemImg);
+                x.itemImage.sprite = itemImg;
+                this.gameObject.SetActive(false);
+            }
+            else
+            {
+                x.inven.Add(this);
+
+                //Instantiate(itemImg);
+                x.itemImage.sprite = itemImg;
+            }
+            this.gameObject.SetActive(false);
+        }
+        else
+            Destroy(this.gameObject);
+
+        //other.gameObject.GetComponent<Inventory>().inventory.Enqueue(this);
         //gameObject.GetComponent<Renderer>().enabled = false;
         //gameObject.GetComponent<Renderer>().enabled = false;
-        this.gameObject.SetActive(false);
     }
 
     public void use()
     {
         Debug.Log("아이템사용");
+        var a = Physics.RaycastAll(playervecter.position, playervecter.forward, 3f, 1 << 10);
+        foreach (var item in a)
+        {
+            Destroy(item.transform.gameObject);
+        }
+        Destroy(this.gameObject);
     }
 }
--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,21 +1,31 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEngine.UI;
 
 [DefaultExecutionOrder(-500)]
 public class Inventory : MonoBehaviour
 {
+    public Image itemImage;
     public List<Item> inven = new List<Item>();
-    public Queue<Item> inventory = new Queue<Item>();
+
+    //public Queue<Item> inventory = new Queue<Item>();
+    public event Action ItemChanged = () => { };
 
     public void use()
     {
-        if (inventory.Count == 0)
+        if (inven[0] == null)
         {
             Debug.Log("아이템다썻음");
+            itemImage.sprite = null;
             return;
         }
-        var a = inventory.Dequeue();
-        a.use();
+        else
+        {
+            inven[0].use();
+            itemImage.sprite = inven[1].itemImg;
+            inven.RemoveAt(0);
+        }
     }
 
     private void Update()
