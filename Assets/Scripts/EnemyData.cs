d521a56541625183268875052fc2ea73d1998702
wkdeo12 <43337006+wkdeo12@users.noreply.github.com>
2019-03-13 18:27:33 +0900

    에너미데이타

---
+++ Assets/Scripts/EnemyData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class EnemyData : MonoBehaviour
+{
+    public float speed;
+    public int hp;
+    public int gold;
+    public Action SomeThing = () => { };
+}
